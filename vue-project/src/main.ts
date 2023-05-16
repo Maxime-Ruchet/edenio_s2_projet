3601fbcc3343734029bf6ed1602fbca9aec50763
Maxime-Ruchet <116868546+Maxime-Ruchet@users.noreply.github.com>
2023-05-16 08:24:47 +0200

    installation tailwin

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,8 +1,11 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+
+import './assets/css/main.css'
+import './assets/css/style.css'
 
 const app = createApp(App)
 
