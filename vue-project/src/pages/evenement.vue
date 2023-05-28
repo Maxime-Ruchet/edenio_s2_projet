cc552b8078e65fe22a43fed3a0bd30cc8cde260a
Maxime-Ruchet <116868546+Maxime-Ruchet@users.noreply.github.com>
2023-05-28 11:43:10 +0200

    modification page événement

--- vue-project/src/pages/evenement.vue
+++ vue-project/src/pages/evenement.vue
@@ -1,25 +1,30 @@
 <template>
-    <h1 class="text-center text-green mt-8">Évenement</h1>
-    <hr class="mt-16">
-<div class="flex items-center">
-  <p class="block ml-4">Saint-Nazair : Une operation ramassage a lieu a Saint-Nazair afin de dépolluer la plage</p>
-  <img src="../public/imageevenement1.webp" alt="" class="text-gray-500 p-4 block rounded-lg w-40">
-</div>
-<p class="block ml-4">Il y a 1 heure</p>
+  <h1 class="text-center text-green mt-8">Évenement</h1>
+  <div class="container mx-auto px-4 py-8">
+    <div class="flex flex-col space-y-8">
+      <div class="bg-white p-4 rounded shadow flex">
+        <img src="../public/imageevenement1.webp" alt="Saint-Nazair" class="h-16 w-16 rounded-md mr-4">
+        <div>
+          <h2 class="text-lg font-semibold text-gray-800">Saint-Nazair : Une opération ramassage a lieu à Saint-Nazair afin de dépolluer la plage</h2>
+          <p class="text-gray-600">Il y a 1 heure</p>
+        </div>
+      </div>
 
-<hr class="mt-2">
-<div class="flex items-center">
-  <p class="block ml-4">Ille sur Têt : Chaque année, l’Art des Jardins cultive sa différence.</p>
-  <img src="../public/imageevenement2.webp" alt="" class="text-gray-500 p-4 block rounded-lg w-40">
-</div>
-<p class="block ml-4">Il y a 3 heures</p>
+      <div class="bg-white p-4 rounded shadow flex">
+        <img src="../public/imageevenement2.webp" alt="Ille sur Têt" class="h-16 w-16 rounded-md mr-4">
+        <div>
+          <h2 class="text-lg font-semibold text-gray-800">Ille sur Têt : Chaque année, l'Art des Jardins cultive sa différence</h2>
+          <p class="text-gray-600">Il y a 3 heures</p>
+        </div>
+      </div>
 
-<hr class="mt-2">
-<div class="flex items-center">
-  <p class="block ml-4">Minervois : La 4ème édition du festival sur le thème de la nature se déroulera le dimanche 5 mars 2023.</p>
-  <img src="../public/imageevenement3.webp" alt="" class="text-gray-500 p-4 block rounded-lg w-40">
-</div>
-<p class="block ml-4">Il y a 9 heures</p>
-
-<hr class="mt-2">
+      <div class="bg-white p-4 rounded shadow flex">
+        <img src="../public/imageevenement3.webp" alt="Minervois" class="h-16 w-16 rounded-md mr-4">
+        <div>
+          <h2 class="text-lg font-semibold text-gray-800">Minervois : La 4ème édition du festival sur le thème de la nature se déroulera le dimanche 5 mars 2023</h2>
+          <p class="text-gray-600">Il y a 9 heures</p>
+        </div>
+      </div>
+    </div>
+  </div>
   </template>
