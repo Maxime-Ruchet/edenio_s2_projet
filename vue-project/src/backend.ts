297f3a2766eff64a7778cbbd11c82060c0f4e03c
Maxime-Ruchet <116868546+Maxime-Ruchet@users.noreply.github.com>
2023-06-04 12:13:23 +0200

    mise en place cardproduits

--- vue-project/src/backend.ts
+++ vue-project/src/backend.ts
@@ -2,6 +2,7 @@ import PocketBase from 'pocketbase' ;
 export const pb = new PocketBase('http://127.0.0.1:8090') ;
 import type { EvenementResponse } from './pocketbase-types';
 import type { GlossaireResponse } from './pocketbase-types';
+import type { ProduitsResponse } from './pocketbase-types';
 
 export async function Allevenement() {
   const records = await pb.collection('evenement').getFullList<EvenementResponse>()
@@ -23,3 +24,14 @@ export async function oneIDglossaire(id: string) {
   const oneRecord = await pb.collection('glossaire').getOne<GlossaireResponse>(id,);
   return oneRecord;
 }
+
+export async function Allproduits() {
+  const records = await pb.collection('produits').getFullList<ProduitsResponse>()
+  return records
+}
+
+export async function oneIDproduits(id: string) {
+  const oneRecord = await pb.collection('produits').getOne<ProduitsResponse>(id,);
+  return oneRecord;
+}
+
---
+++ vue-project/src/components/Cardproduits.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { ProduitsResponse } from '@/pocketbase-types'
+const props: ProduitsResponse = defineProps<ProduitsResponse>()
+import {pb} from '@/backend' 
+const urlImg0 = props.image ? pb.getFileUrl(props, props.image, { thumb: '100x250' }) : '/image-not-found.png'
+console.log(urlImg0)
+import { oneIDproduits } from '@/backend'
+const unproduits = await oneIDproduits(props.id)
+</script>
+
+<template>
+    <div class="container mx-auto px-4 py-8">
+  <div class="grid grid-cols lg:grid-cols-2 gap-8">
+      <img class="m-auto pb-4 w-24" :src="urlImg0">
+  <p class="ml-4" v-html=" unproduits.image "></p>
+    <p class="ml-4" v-html=" unproduits.titre"></p>
+    <p class="ml-4" v-html=" unproduits.prix"></p>
+    </div>
+    </div>
+  </template>
